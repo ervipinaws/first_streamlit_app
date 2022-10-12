8abf8447fd33379429bd87d0c6f69e8a51fbd3c7
Vipin Gupta <115560971+ervipinaws@users.noreply.github.com>
2022-10-12 08:06:09 +0530

    Update streamlit_app.py
    get user input for fruit and fetch the data from fruityvice for same.

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,10 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 import requests
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #it only displays text
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+#it only displays text --> streamlit.text(fruityvice_response.json()) 
 
 # format json output as a table 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
