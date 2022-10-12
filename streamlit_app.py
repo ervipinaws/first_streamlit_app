2871351da7aa76d85ef5a2177f633d675edcee7c
Vipin Gupta <115560971+ervipinaws@users.noreply.github.com>
2022-10-12 07:55:10 +0530

    Update streamlit_app.py
    change response to display in JSON format.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit
 import pandas
-import requests
 
 streamlit.title('My Parents New Healthy Dinner')
 
@@ -29,5 +28,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+
+#New section to display fruityvice api response
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.jason())
