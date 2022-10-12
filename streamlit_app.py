565bd719e288625df2d2097cb0de20f0dcc55e1e
Vipin Gupta <115560971+ervipinaws@users.noreply.github.com>
2022-10-12 13:11:05 +0530

    Update streamlit_app.py
    bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -129,7 +129,7 @@ except URLError as e:
 
 ########################################################################################33
 
-def get_fruit_load_list()
+def get_fruit_load_list():
     with my_cnx.cursor() as my cur:
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
