d89967a868ddbaa57c2c4d11fe4ec7bcfe47e424
Vipin Gupta <115560971+ervipinaws@users.noreply.github.com>
2022-10-12 11:53:03 +0530

    Update streamlit_app.py
    bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,6 +58,6 @@ streamlit.header("The fruit load list contains:")
 #streamlit.dataframe(my_data_row)
 streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add')
+add_my_fruit = streamlit.text_input('What fruit would you like to add','Papaya')
 streamlit.write('The user entered ', add_my_fruit)
 my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values ('" + add_my_fruit + "')")
